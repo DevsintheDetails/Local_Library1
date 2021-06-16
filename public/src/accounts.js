aca892363a349dfedabb9a2ae625ee5ad9fe4cdb
Cody <parker.cody112@gmail.com>
2021-06-16 11:53:07 -0400

    Initialized repository and completed getMostPopAuthors

---
+++ public/src/accounts.js
@@ -0,0 +1,48 @@
+function findAccountById(accounts, id) {
+  let found = accounts.find((account)=> account.id === id)
+  return (!found) ? null: found;
+}
+
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1:-1);
+}
+
+
+function getTotalNumberOfBorrows(account, books) {
+  let counter = 0;
+  books.forEach((book)=> {
+    book.borrows.forEach((transaction)=> {
+      if(transaction.id === account.id) counter++;
+    });
+  });
+  return counter;
+  }
+
+  
+  function getBooksPossessedByAccount(account, books, authors) {
+    const array = [];
+books.forEach((book) => {
+  const borrowed = book.borrows.filter(
+    (borrow) => borrow.id === account.id && borrow.returned === false
+    );
+    if (borrowed.length > 0) {
+      const found = authors.find((author) => book.authorId === author.id);
+      array.push({
+        id: book.id,
+        title: book.title,
+        genre: book.genre,
+        authorId: book.authorId,
+        author: found,
+        borrows: borrowed,
+      });
+    }
+});
+return array;
+  }
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
---
+++ public/src/home.js
@@ -0,0 +1,68 @@
+function getTotalBooksCount(books) {
+  return books.length;
+}
+
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
+
+function getBooksBorrowedCount(books) {
+  return books.filter((book) => !book.borrows[0].returned).length
+}
+
+function getMostCommonGenres(books) {
+  let genres = [];
+  books.forEach((book) => {
+    let genreIndex = genres.findIndex((genre) => {
+      return genre.name === book.genre;
+    });
+    if (genreIndex !== -1) {
+      genres[genreIndex].count++;
+    } else {
+      genres.push({ name: book.genre, count: 1 });
+    }
+  });
+  genres.sort((a, b) => b.count - a.count);
+  return genres.slice(0, 5);
+} 
+
+function getMostPopularBooks(books) {
+  let popularityBooks = [];
+    books.forEach((book) => {
+      popularityBooks.push({"name": book.title, "count": book.borrows.length});
+    });
+  popularityBooks.sort((a, b) => b.count - a.count);
+ 
+  let numberItems=5;
+  return popularityBooks.slice(0, numberItems);    
+}
+
+function getMostPopularAuthors(books, authors) {
+  let authorCounts = books.reduce((acc, book) => {
+if (!acc[book.authorId]) {
+  acc[book.authorId] = book.borrows.length
+} else {
+  acc[book.authorId] += book.borrows.length
+}
+return acc;
+  } ,{});
+  return Object.keys(authorCounts)
+    .sort((id1, id2) => authorCounts[id2] - authorCounts[id1])
+    .slice(0, 5)
+    .map((key) => {
+    let author = authors.find((author) => {
+      return author.id === parseInt(key);
+    })
+    let name = `${author.name.first} ${author.name.last}`
+    return {name, count: authorCounts[key]}
+  })
+}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
